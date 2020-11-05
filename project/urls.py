9174b7081b1c13a49baa5c09cb3d3978ed7286b4
hermanTenuki <boss.warning@gmail.com>
2020-11-05 19:13:22 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -14,19 +14,20 @@ handler400 = 'app.views.handler400_view'
 handler404 = 'app.views.handler404_view'
 handler500 = 'app.views.handler500_view'
 
-# If DEBUG is True - allow to visit error pages
+# If DEBUG is True:
 if settings.DEBUG:
+    # Allow to visit error pages
     from app.views import handler400_view, handler404_view, handler500_view
-
     urlpatterns += [
         path('400/', handler400_view),
         path('404/', handler404_view),
         path('500/', handler500_view)
     ]
 
-# If DEBUG is True - turn on django's admin page and manually serve static/media files
-if settings.DEBUG:
+    # Turn on django's admin page and manually serve static/media files
     urlpatterns.append(path('admin/', admin.site.urls))
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns.append(path('rosetta/', include('rosetta.urls')))  # Also add "rosetta" for translation
+
+    # Also add "rosetta" for translation
+    urlpatterns.append(path('rosetta/', include('rosetta.urls')))
