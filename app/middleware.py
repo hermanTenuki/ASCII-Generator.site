18df402d19c3871b19894e1aa83b1aad34a349a1
hermanTenuki <itseasy322@gmail.com>
2021-07-11 11:34:11 +0300

    Deleted ForceDefaultLanguageMiddleware middleware

--- app/middleware.py
+++ app/middleware.py
@@ -3,22 +3,6 @@ from django.conf import settings
 from django.utils import translation
 
 
-class ForceDefaultLanguageMiddleware(MiddlewareMixin):
-    """
-    Ignore Accept-Language HTTP headers
-
-    This will force the I18N machinery to always choose settings.LANGUAGE_CODE
-    as the default initial language, unless another one is set via sessions or cookies
-
-    Should be installed *before* any middleware that checks request.META['HTTP_ACCEPT_LANGUAGE'],
-    namely django.middleware.locale.LocaleMiddleware
-    """
-
-    def process_request(self, request):
-        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
-            del request.META['HTTP_ACCEPT_LANGUAGE']
-
-
 class LanguageURLSpecifyMiddleware(MiddlewareMixin):
     """
     Checks if url is containing language code like "domain.com/ru/something/",
